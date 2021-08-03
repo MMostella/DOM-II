c9b2855225c928b55188d55e008137d625f9b0d9
MMostella <masonmostella@gmail.com>
2021-08-03 17:00:35 -0500

    10 Done

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ document.addEventListener('keydown', hKey);
 
 const funBus = document.querySelector('.logo-heading');
 
-funBus.addEventListener('mouseover', function(event){
+funBus.addEventListener('mouseover', event => {
     funBus.style.fontSize = '60px';
     funBus.style.color = 'red';
 })
@@ -35,7 +35,7 @@ function myFunction() {
 const busPic = document.querySelector('.intro img');
 
 busPic.addEventListener('dblclick', function(event){
-    busPic.style.border = '2px solid black';
+    this.style.border = '2px solid black';
 })
 
 // 5
@@ -52,6 +52,120 @@ signMeUp.addEventListener('mouseleave', function(event){
 
 // 6
 
-window.addEventListener('resize', function(event){
+window.addEventListener('resize', event => {
     alert('You changed my size!!')
 })
+
+// 7
+
+const test = document.querySelector('.content-section');
+
+test.addEventListener('mousemove', event => {
+    test.style.backgroundColor = 'lightBlue';
+})
+
+// 8
+
+const fooTer = document.querySelector('.footer p');
+
+fooTer.addEventListener('mousedown', event => {
+    console.log('You clicked the footer!!');
+})
+
+// 9
+// Z
+const zItemDown = document.querySelector('.intro');
+
+function zKeyDown(event){
+    if (event.key === 'z'){
+        zItemDown.style.fontSize = '35px';
+    }
+}
+
+document.addEventListener('keydown', zKeyDown);
+
+const zItemUp = document.querySelector('.intro');
+
+function zKeyUp(event){
+    if (event.key === 'z'){
+        zItemUp.style.fontSize = '20px';
+    }
+}
+
+document.addEventListener('keyup', zKeyUp);
+
+// X
+
+const xItemDown = document.querySelector('.text-content');
+
+function xKeyDown(event){
+    if (event.key === 'x'){
+        xItemDown.style.fontSize = '70px';
+    }
+}
+
+document.addEventListener('keydown', xKeyDown);
+
+const xItemUp = document.querySelector('.text-content');
+
+function xKeyUp(event){
+    if (event.key === 'x'){
+        xItemUp.style.fontSize = '25px';
+    }
+}
+
+document.addEventListener('keyup', xKeyUp);
+
+// C
+
+const cItemDown = document.querySelector('.content-destination');
+
+function cKeyDown(event){
+    if (event.key === 'c'){
+        cItemDown.style.fontSize = '50px';
+    }
+}
+
+document.addEventListener('keydown', cKeyDown);
+
+const cItemUp = document.querySelector('.content-destination');
+
+function cKeyUp(event){
+    if (event.key === 'c'){
+        cItemUp.style.fontSize = '25px';
+    }
+}
+
+document.addEventListener('keyup', cKeyUp);
+
+// V
+
+const vItemDown = document.querySelector('footer');
+
+function vKeyDown(event){
+    if (event.key === 'v'){
+        vItemDown.style.fontSize = '80px';
+    }
+}
+
+document.addEventListener('keydown', vKeyDown);
+
+const vItemUp = document.querySelector('footer');
+
+function vKeyUp(event){
+    if (event.key === 'v'){
+        vItemUp.style.fontSize = '20px';
+    }
+}
+
+document.addEventListener('keyup', vKeyUp);
+
+// 10
+
+window.addEventListener('load', event => {
+    console.log('Window was just loaded!!!')
+})
+
+
+// Propagation
+
